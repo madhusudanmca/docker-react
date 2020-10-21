2709711c37280e18e6e6716b437398a512188683
Sudan <M_Sudan@Dell.com>
2020-10-21 13:48:00 +0530

    added aws congiguration

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I'm running now, but updated!
+          I'm running now, but in cloud!
         </p>
         <a
           className="App-link"
